3e92b1eb119c3fb543a2840c99bfaa778b0ec6fb
Luke O'Neill <luke@yearbookmachine.com>
2015-12-27 11:08:53 +0000

    set up webpack, react and babel for building client for the rankings predictor.

---
+++ src/components/PulseballRankingsPredictor.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default React.createClass({
+
+	render: function() {
+		return (
+			<div>
+				<h1>Pulseball Rankings Predictor</h1>
+				<p>This is the new rankings predictor for Pulseball</p>
+			</div>	
+		);
+	}
+
+});
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PulseballRankingsPredictor from './components/PulseballRankingsPredictor';
+
+ReactDOM.render(
+  <PulseballRankingsPredictor />,
+  document.getElementById('app')
+);
